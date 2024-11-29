541e5b2b0688d224bd77439586c3a9f9e5f2998d
toufiq007 <t.limon2342@gmail.com>
2024-11-29 21:49:54 +0600

    add root node and nested node in sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,10 @@
 import Link from "next/link";
 
 const SidebarNav = ({ docs }) => {
+  // find root nodes
   const roots = docs.filter((doc) => !doc.parent);
+
+  // group nonroots nodes
   const nonRoots = Object.groupBy(
     docs.filter((doc) => doc.parent),
     ({ parent }) => parent
@@ -22,6 +25,21 @@ const SidebarNav = ({ docs }) => {
                   >
                     <span className="truncate">{root.title}</span>
                   </Link>
+                  {nonRoots[root.id] && (
+                    <ul role="list" className="border-l border-transparent">
+                      {nonRoots[root.id].map((subRoot) => (
+                        <li key={subRoot.id}>
+                          <Link
+                            aria-current="page"
+                            className="flex justify-between gap-2 py-1 pl-8 pr-3 text-sm text-zinc-900 transition dark:text-white"
+                            href={`/docs/${root.id}/${subRoot.id}`}
+                          >
+                            <span className="truncate">{subRoot.title}</span>
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
                 </li>
               </>
             );
